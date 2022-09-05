efe8f549a852ca76028486fa1bff1501b2b6335b
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2022-09-06 01:44:38 +0700

    update config to hosting

--- app/config/database.js
+++ app/config/database.js
@@ -14,14 +14,11 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "wtsodypvfhlaar",
-    "password": "5cd3343c1a975445345b212a4c3cedd78bbdfac9cfb24e4832ce44e7496893c1",
-    "database": "de17sjn19vrcbu",
-    "host": "ec2-44-210-36-247.compute-1.amazonaws.com",
-    "dialect": "postgres",
-    "port": 5432,
-    "dialectOptions": {
-      "ssl": { "rejectUnauthorized": false }
-    }
+    "username": "almardiy_iseplutpi",
+    "password": "almardiy_iseplutpi",
+    "database": "almardiy_clone_trello",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "port": 3306,
   }
 }

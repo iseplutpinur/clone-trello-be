df9f5d94a7f6200edc84d475b78f0150407e11b4
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2022-09-06 00:47:20 +0700

    add cors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require('cors');
 const todosRouter = require('./app/api/todos/router');
 const itemsRouter = require('./app/api/items/router');
 
@@ -10,6 +10,7 @@ const app = express();
 
 const URL = '/api/v1';
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

b4249d7b6e8185e5cd960c40fd739a49d13d2ed4
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2022-09-06 01:14:02 +0700

    update config to heroku

--- app/config/database.js
+++ app/config/database.js
@@ -14,10 +14,14 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": "wtsodypvfhlaar",
+    "password": "5cd3343c1a975445345b212a4c3cedd78bbdfac9cfb24e4832ce44e7496893c1",
+    "database": "de17sjn19vrcbu",
+    "host": "ec2-44-210-36-247.compute-1.amazonaws.com",
+    "dialect": "postgres",
+    "port": 5432,
+    "dialectOptions": {
+      "ssl": { "rejectUnauthorized": false }
+    }
   }
 }
